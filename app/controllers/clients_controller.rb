406202b3cf070c0e1dbc44246de1423526c5a24a
Jonathan Berkowitz <jberkowitz@MacBook-Pro.local>
2018-06-27 10:34:14 -0400

    create basic clients and add them to user

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,10 +2,30 @@ class ClientsController < ApplicationController
 
   def index
   end
-  
+
   def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    @user = current_user
+
+    if @client.save
+      @user.clients << @client
+      redirect_to @user
+    else
+      redirect_to '/new'
+    end
   end
 
   def show
   end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name)
+  end
+
 end
