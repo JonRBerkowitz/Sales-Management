1f5f8a44f3055ce32fe5f3841ae35d1faba03656
Jonathan Berkowitz <jberkowitz@MacBook-Pro.local>
2018-06-27 11:21:20 -0400

    add basic contact creation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,10 +2,28 @@ class ContactsController < ApplicationController
 
   def index
   end
-  
+
   def new
+    @contact = Contact.new
+    @clients = current_user.clients
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to current_user
+    else
+      redirect_to '/new'
+    end
   end
 
   def show
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :client_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_many :clients
   has_many :contacts, through: :clients
-  has_many :appointments
-  has_many :contacts, through: :appointments
 
   has_secure_password
 end
