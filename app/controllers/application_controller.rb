7b4d2657a4591fe534c17b765f68ca0d625290d4
Jonathan Berkowitz <jberkowitz@MacBook-Pro-2.local>
2018-08-18 19:18:49 -0400

    add redirect for logged out users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      redirect_to '/login'
+    end
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,10 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def login_redirect
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
 end
